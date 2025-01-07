2a24c829fff3df26ed29c0802d788821ec2fe281
Wafaa Mohamed <121331529+Wafaamohamed2@users.noreply.github.com>
2025-01-07 21:50:16 +0200

    Add project files.

---
+++ Binding/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using Binding.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVC_1.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+
+        public string? Address {  get; set; }
+
+        public string Image { get; set; }
+
+        [ForeignKey("Department")] 
+        public int Dept_Id { get; set; }
+
+        // virtual to make refrence of last one not new object
+        public virtual Department? Department { get; set; }
+    }
+}
